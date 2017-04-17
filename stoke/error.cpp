8433bc1fa9690d990af28bed9f9597244c0d4dd3
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2017-04-17 12:45:23 +0530

    [stoke] works for simple testcases

--- stoke/error.cpp
+++ stoke/error.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
 #include "../run_program.h"
 
 typedef pair<int8_t, int8_t> testpoint;
+double beta = 2.0;
+
+const int REGISTER_LIMIT = 2;
+const int NUM_RESTARTS = 10;
 
 int8_t popcount(uint8_t num) {
     int8_t ans = 0;
+    int8_t penalty = 1;
     while(num != 0) {
-        ans += (num & 1);
+        ans += (penalty) * (num & 1);
         num >>= 1;
+        penalty++;
     }
     return ans;
 }
@@ -43,20 +52,173 @@ int total_reg_error(const vector<testpoint> &testcase,
     return error;
 }
 
-int main() {
-    vector<testpoint> testcase;
-    vector<instr_t> program (1);
+inline void change_opcode_2_registers(instr_t &instr) {
+    const int opcodes[] = {MOV, ADD, SUB/*, ADC, SBC, AND, OR, EOR*/};
+    const int num_opcodes = sizeof(opcodes) / sizeof(int);
+    // cout << "(" << instr.opcode;
+    instr.opcode = opcodes[rand() % num_opcodes];
+    // cout << ", " << instr.opcode << ")\n";
+}
 
-    program[0].opcode = ADD;
-    program[0].reg_d = 0;
-    program[0].reg_r = 0;
+inline void change_opcode(instr_t &instr) {
+    switch(instr.opcode) {
+        case MOV:
+        case ADD:
+        case SUB:
+        case ADC:
+        case SBC:
+        case AND:
+        case OR:
+        case EOR:
+            change_opcode_2_registers(instr);
+            break;
+    }
+}
+
+inline void change_dest_register(instr_t &instr) {
+    switch(instr.opcode) {
+        case MOV:
+        case ADD:
+        case ADC:
+        case SUB:
+        case SBC:
+        case AND:
+        case OR:
+        case EOR:
+            // source register can be r0 .. r3
+            instr.reg_d = rand () % REGISTER_LIMIT;
+    }
+}
+
+inline void change_src_register(instr_t &instr) {
+    switch(instr.opcode) {
+        case MOV:
+        case ADD:
+        case SUB:
+        case ADC:
+        case SBC:
+        case AND:
+        case OR:
+        case EOR:
+            // source register can be r0 .. r3
+            instr.reg_r = rand () % REGISTER_LIMIT;
+    }
+}
+
+
+void mcmc_search(const vector<testpoint> &testcase, 
+        vector<instr_t> &program) {
+
+    int prevcost = total_reg_error(testcase, program);
+    for(int i = 0; i < 10000; ++i) {
+        int prog_index = rand() % program.size();
+        // printf("pi = %d:\t", prog_index);
+        instr_t previnstr = program[prog_index];
+
+        int decider = rand () % 100;
+        if(decider < 90) {
+            // printf("op:");
+            change_opcode(program[prog_index]);
+        }
+        else if(decider < 95) {
+            // printf("dr:");
+            change_dest_register(program[prog_index]);
+        }
+        else {
+            // printf("sr:");
+            change_src_register(program[prog_index]);
+        }
+
+        int newcost = total_reg_error(testcase, program);
+
+        // printf("\t%d ---> %d\n", prevcost, newcost);
+        float accept = min(1.0, exp(-beta * newcost / prevcost));
+        // printf("acc = %lf\t", accept);
+
+        if(rand() % 100000 < accept * 100000) {
+            prevcost = newcost;
+            //printf("ACCEPTED\n");
+        }
+        else {
+            program[prog_index] = previnstr;
+            // printf("REJECTED\n");
+        }
+    }
+}
+
+void print_instr(const instr_t &instr) {
+    switch(instr.opcode) {
+        case ADD:
+            printf("ADD %d %d\n", instr.reg_r, instr.reg_d);
+            break;
+        case SUB:
+            printf("SUB %d %d\n", instr.reg_r, instr.reg_d);
+            break;
+        case MOV:
+            printf("MOV %d %d\n", instr.reg_r, instr.reg_d);
+            break;
+        default:
+            printf("WTF!!!");
+
+    }
+}
+
+
+void test_subtraction() {
+    state_t state;
+    clear_state(state);
+    state.reg[0] = 32;
+    state.reg[1] = 64;
+
+
+    instr_t instr;
+    instr.opcode = SUB;
+    instr.reg_d = 0;
+    instr.reg_r = 1;
+
+    print_bitform(state.reg[0]);
+    print_bitform(state.reg[1]);
+    run_arith_logic(state, instr);
+    print_bitform(state.reg[0]);
+    print_bitform(state.reg[1]);
+
+}
+
+int main() {
+    // test_subtraction(); return 0;
+    srand(time(NULL));
+    vector<testpoint> testcase;
+    vector<instr_t> program(10);
 
     /*
+    program[0].opcode = MOV;
+    program[0].reg_d = 1;
+    program[0].reg_r = 0;
+
     program[1].opcode = ADD;
     program[1].reg_r = 1;
     program[1].reg_d = 0;
-    */
 
-    read_testcase("../testcase/exp_testcase.txt", testcase);
-    std::cout << total_reg_error(testcase, program) << "\n";
+    program[2].opcode = ADD;
+    program[2].reg_r = 1;
+    program[2].reg_d = 0;
+    */
+    read_testcase("../testcase/ml7_testcase.txt", testcase);
+
+    int opcodes [] = {ADD, SUB, MOV};
+    int num_opcodes = sizeof(opcodes) / sizeof(int);
+    for(int restarts = 0; restarts < NUM_RESTARTS; ++restarts) {
+        for(auto &instr: program) {
+            instr.opcode = opcodes[rand() % num_opcodes];
+            instr.reg_d = rand() % REGISTER_LIMIT;
+            instr.reg_r = rand() % REGISTER_LIMIT;
+        }
+        cout << "first = " << total_reg_error(testcase, program) << "\n";
+        mcmc_search(testcase, program);
+        cout << "final = " << total_reg_error(testcase, program) << "\n";
+        for(auto &instr: program) {
+            print_instr(instr);
+        }
+    }
+
 }
--- testcase/testcase_gen.cpp
+++ testcase/testcase_gen.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <cassert>
+#include "../run_program.h"
 using namespace std;
 
 const double fixed_point = 1 / double(32);
@@ -27,17 +28,19 @@ int float_to_fixed(float fl_num) {
 
 int main() {
     build_mapping();
+    /*
     for(int i = 0; i <= 255; ++i) {
         cout << i << "\t" << mapping[i] << "\t" << float_to_fixed(mapping[i]) << "\n";
     }
+    */
     for(int i = 0; i <= 255; ++i) {
         float flx = mapping[i];
-        float fly = flx * 2; //exp(flx);
+        float fly = flx * 7; //exp(flx);
         int fx = i - 128; 
         int fy = float_to_fixed(fly) - 128;
         assert(-128 <= fx and fx <= 127);
         assert(-128 <= fy and fy <= 127);
-        if(-1 <= flx and flx <= 1)
+        if(-1 <= flx and flx <= 1 and -100 < fy and fy < 100)
             cout << fx << " " << fy << "\n";
     }
 }
